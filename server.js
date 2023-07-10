94974b5bac5d3c0b619a229a30c6b31d7d8ca667
Adrien Gagnon <adrien.gagnon25@outlook.com>
2023-07-09 20:58:09 -0400

    changed package for render

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 
+(function () {
+    var childProcess = require('child_process');
+    var oldSpawn = childProcess.spawn;
+    function mySpawn() {
+        console.log('spawn called');
+        console.log(arguments);
+        var result = oldSpawn.apply(this, arguments);
+        return result;
+    }
+    childProcess.spawn = mySpawn;
+})();
+
 process.on('uncaughtException', (err) => {
     console.log('uncaught exception, shutting down...');
     console.log(err.name, err.message);
